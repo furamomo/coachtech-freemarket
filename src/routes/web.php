7714bd2ae6407638eb7391c10bc495c5d349ccb5
yamada mayu <furamomo.NNN.0911@outlook.jp>
2026-02-21 22:55:34 +0900

    web.php README更新

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Auth\EmailVerificationRequest;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ItemController;
